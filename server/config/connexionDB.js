8d1d256f2410c8cba9d82741e922c5cf45d6ca1b
EA70 <eliekoyabanda@gmail.com>
2026-06-26 10:26:28 +0200

    Configuration neon

--- server/config/connexionDB.js
+++ server/config/connexionDB.js
@@ -3,14 +3,24 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+
 const pool = new Pool({
-  user: process.env.DB_USER,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  connectionTimeoutMillis: 10000, 
+});
+
+
+/** 
+ * 
+ * const pool = new Pool({
+user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
   //connectionString: process.env.DATABASE_URL, ssl: {rejectUnauthorized: false,},
-});
+}); */
 
 pool
   .connect()
